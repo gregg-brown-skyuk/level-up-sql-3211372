fe0967c5490a26eb2c240277d18c455eec02af23
Gregg Brown <101634622+gregg-brown-skyuk@users.noreply.github.com>
2023-05-12 14:22:59 +0000

    01_11 & 01_12

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -1,4 +1,24 @@
 -- Update information in the database.
-
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+select d.DishID
+from dishes d
+where d.Name = 'Quinoa Salmon Salad';
+
+update customers
+set FavoriteDish = 9
+where CustomerID = 42;
+
+select
+  c.CustomerID,
+  c.FirstName,
+  c.LastName,
+  d.DishID,
+  d.Name
+from
+  customers c,
+  Dishes d
+where
+  c.FavoriteDish = d.DishID
+  and c.FirstName = 'Cleo';
--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,11 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+with ordercount as (select o.CustomerID, count(o.OrderID) as ordercount
+from orders o
+group by o.CustomerID
+having ordercount >= 14)
+select c.FirstName, c.LastName, c.Email, oc.ordercount 
+from customers c, ordercount oc
+where c.CustomerID = oc.CustomerID
+order by oc.ordercount desc;
