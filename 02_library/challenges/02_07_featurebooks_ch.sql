20ee10e7d2b9247abc7172937d79b85935c1b74b
Gregg Brown <101634622+gregg-brown-skyuk@users.noreply.github.com>
2023-07-14 12:58:56 +0000

    02-07

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,21 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+select
+  b.Barcode,
+  b.Title,
+  b.Author
+from
+  books b
+where
+  b.Published between 1890 and 1900
+  and b.BookID not in (
+  select
+    l.BookID
+  from
+    loans l
+  where
+    l.ReturnedDate is null
+)
+order by b.Title asc
